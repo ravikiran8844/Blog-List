ceee7d2590f6a05517fa3630479a58345a906875
root <ravi.kiran8844@.com>
2022-09-30 10:12:28 +0000

    first commit

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {details} = props
+  const {title, publishedDate, description} = details
+  return (
+    <li>
+      <div className="title">
+        <h1>{title}</h1>
+        <p>{publishedDate}</p>
+      </div>
+      <div>
+        <p>{description}</p>
+      </div>
+    </li>
+  )
+}
+export default BlogItem
